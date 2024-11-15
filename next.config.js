ecba021ff4cdc8fc335b1b61f25f03bd544d34c7
tnynhi <truongnguyenyennhi.work@gmail.com>
2024-11-15 17:50:52 +0700

    edit file next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/nhitnyBlog.github.io',
-    assetPrefix: '/nhitnyBlog.github.io/',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  basePath: '/nhitnyBlog.github.io',
+  assetPrefix: '/nhitnyBlog.github.io/',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
